72bbdf148c47c1be071454bc34a9c5b0809a373f
Vicki Boykis <vicki.boykis@gmail.com>
2018-02-07 22:35:37 -0500

    Test connecting to Twitter API and pulling in tweets

--- soviet-art-bot/run_bot.py
+++ soviet-art-bot/run_bot.py
@@ -1,5 +1,10 @@
-import os
+"""
+Connects to Twitter
+
+"""
 import tweepy
+
+import settings
 
 def twitter_api():
     access_token = settings.ACCESS_TOKEN
@@ -7,7 +12,19 @@ def twitter_api():
     consumer_key = settings.CONSUMER_KEY
     consumer_secret = settings.CONSUMER_SECRET
 
-    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = API(auth)
-    return api
+
+    api = tweepy.API(auth)
+
+
+    public_tweets = api.home_timeline()
+
+    print("running...")
+
+    for tweet in public_tweets:
+        print(tweet.text)
+
+
+
+twitter_api()
