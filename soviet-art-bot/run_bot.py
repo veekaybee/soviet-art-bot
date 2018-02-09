1e3836abb60681926113d07835eb6db2dd3022e5
Vicki Boykis <vicki.boykis@gmail.com>
2018-02-08 20:49:56 -0500

    Testing upload of statically coded file to Twitter

--- soviet-art-bot/run_bot.py
+++ soviet-art-bot/run_bot.py
@@ -4,9 +4,10 @@ Connects to Twitter
 """
 import tweepy
 
+
 import settings
 
-def twitter_api():
+def access_twitter_api():
     access_token = settings.ACCESS_TOKEN
     access_token_secret = settings.ACCESS_SECRET
     consumer_key = settings.CONSUMER_KEY
@@ -16,15 +17,12 @@ def twitter_api():
     auth.set_access_token(access_token, access_token_secret)
 
     api = tweepy.API(auth)
+    return api
+
+def post_tweet(api):
+    api.update_with_media('/Users/vboykis/Desktop/soviet-art-bot/assetsyouth-of-the-poetess-1967.jpg', status="\"Youth of the poetess\"\nPainter, 1967")
+    print("Tweet posted")
 
 
-    public_tweets = api.home_timeline()
-
-    print("running...")
-
-    for tweet in public_tweets:
-        print(tweet.text)
-
-
-
-twitter_api()
+api = access_twitter_api()
+post_tweet(api)
