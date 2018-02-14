5d0a009f75cc5ae055aad8abd6dde317279af083
Vicki Boykis <vicki.boykis@gmail.com>
2018-02-14 15:17:21 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'soviet-art-bot'
+DESCRIPTION = 'A bot that tweets out socialist realism paintings every so often. '
+URL = 'https://github.com/veekaybee/soviet-art-bot'
+EMAIL = 'vickiboykis@gmail.com'
+AUTHOR = 'Vicki Boykis'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # 'requests', 'maya', 'records',
+    ]
+
+    # The rest you shouldn't have to touch too much :)
+    # ------------------------------------------------
+    # Except, perhaps the License and Trove Classifiers!
+    # If you do change the License, remember to change the Trove Classifier for that!
+
+    here = os.path.abspath(os.path.dirname(__file__))
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    #version=about['__version__'],
+    description=DESCRIPTION,
+    # long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+
+)
