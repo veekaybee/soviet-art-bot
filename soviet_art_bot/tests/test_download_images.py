26913fc7b74ccf7118336406f99f0e76fcb2ff15
Vicki Boykis <vicki.boykis@gmail.com>
2018-02-16 11:12:19 -0500

    Refactoring JSON download to accomodate unit testing

--- soviet_art_bot/tests/test_download_images.py
+++ soviet_art_bot/tests/test_download_images.py
@@ -2,18 +2,25 @@ import pytest
 import os
 
 from soviet_art_bot import download_images as dl
-from soviet_art_bot import settings
+import settings
 
 
-# Tests that WikiArt responds with a JSON object
-@pytest.mark.order1
-def test_json():
-    assert type(dl.get_json()) == list
+# Test JSON
+single_painting = [{'artistName': 'Aleksandr Deyneka', 'images': None, 'width': 634, 'artistUrl': '/en/aleksandr-deyneka', 'albums': None, 'title': 'Winter in Kursk', 'id': '577271cfedc2cb3880c2de61', 'year': '1916', 'height': 750, 'paintingUrl': '/en/aleksandr-deyneka/winter-in-kursk-1916', 'flags': 2, 'image': 'https://uploads8.wikiart.org/images/aleksandr-deyneka/winter-in-kursk-1916.jpg', 'map': '0123**67*'}]
+add_painting = [{'artistName': 'Konstantin Yuon', 'images': None, 'width': 776, 'artistUrl': '/en/konstantin-yuon', 'albums': None, 'title': 'The Symphony of Action', 'id': '57727792edc2cb3880d4f131', 'year': '1922', 'height': 650, 'paintingUrl': '/en/konstantin-yuon/the-symphony-of-action-1922', 'flags': 2, 'image': 'https://uploads4.wikiart.org/images/konstantin-yuon/the-symphony-of-action-1922.jpg', 'map': '0123**67*'}]
 
-# Tests that JSON file is saved correctly
-@pytest.mark.order2
-def test_save_json():
-    assert settings.ASSET_PATH.joinpath(settings.JSON_FILE+settings.JSON_SUFFIX).is_file()
+
+def test_build_url():
+    url = settings.BASE_URL + settings.STYLE_URL + "&" + settings.PAGINATION_URL + str(1)
+    assert url == "https://www.wikiart.org/en/paintings-by-style/socialist-realism?json=2&page=1"
+
+
+# Tests that paintings are extracted and added to a list
+def test_parse_data():
+    dl.parse_data(single_painting, add_painting)
+    assert len(single_painting) == 2
+
+
 
 
 
