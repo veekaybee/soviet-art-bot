a6ebe8f8568bb51a612ffcca85e0780dfe0f781f
Vicki Boykis <vicki.boykis@gmail.com>
2018-03-20 12:35:01 -0400

    modifying aws cred file

--- ecr_credentials.sh
+++ ecr_credentials.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env bash
-
 mkdir -p ~/.aws
 
-cat > ~/.aws/credentials << EOL
+cat << EOF > ~/.aws/credentials
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
-EOL
+EOF
