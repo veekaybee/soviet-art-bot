4819e92546dde8958000124fef8e38af92661942
Vicki Boykis <vicki.boykis@gmail.com>
2018-03-20 12:45:23 -0400

    modifying yaml yet again

--- ecr_credentials.sh
+++ ecr_credentials.sh
@@ -1,7 +1,11 @@
 mkdir -p ~/.aws
 
+echo "mkdir"
+
 cat << EOF > ~/.aws/credentials
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOF
+
+cat ~/.aws/credentials
