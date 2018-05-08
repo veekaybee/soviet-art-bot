dd552ae420654397eae6bc985528c66d34f948d5
Vicki Boykis <vicki.boykis@gmail.com>
2018-05-08 14:57:40 -0400

    spelling

--- build_env.sh
+++ build_env.sh
@@ -4,11 +4,11 @@ FILE=${lambda_project_home}/.env
 
 
 if [[ $TRAVIS_BRANCH == 'dev' ]]; then
-aws ssm get-parameters-by-path --path "/Dev" --region u'us-east-1'  |jq -r '.Parameters[]|"\(.Name)=\(.Value)"'|sed -e 's|/Dev/||g' > $FILE
+aws ssm get-parameters-by-path --path "/Dev" --region 'us-east-1'  |jq -r '.Parameters[]|"\(.Name)=\(.Value)"'|sed -e 's|/Dev/||g' > $FILE
     echo "=========== $TRAVIS_BRANCH ENV ==========="
     cat $FILE
 elif [[ $TRAVIS_BRANCH == 'master' ]]; then
-aws ssm get-parameters-by-path --path "/Master" --region u'us-east-1' |jq -r '.Parameters[]|"\(.Name)=\(.Value)"'|sed -e 's|/Master/||g' > $FILE
+aws ssm get-parameters-by-path --path "/Master" --region 'us-east-1' |jq -r '.Parameters[]|"\(.Name)=\(.Value)"'|sed -e 's|/Master/||g' > $FILE
     echo "=========== $TRAVIS_BRANCH ENV ==========="
     cat $FILE
 else
